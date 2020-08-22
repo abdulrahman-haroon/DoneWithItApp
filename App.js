3fb1514608756db5f5eeda310439530094574b86
abdulrahman-haroon <abdulrahman.haroonr@gmail.com>
2020-08-22 18:02:25 +0500

    Navigations added

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { StyleSheet } from "react-native";
-import ListEditScreen from "./app/screens/ListEditScreen";
+
 import { NavigationContainer } from "@react-navigation/native";
+
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import NavigationTheme from "./app/navigation/NavigationTheme";
 import AppNavigator from "./app/navigation/AppNavigation";
