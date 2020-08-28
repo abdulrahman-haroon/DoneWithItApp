531b75cf79a91504ca3f6143817a8435891836b7
Abdul Rahman Haroon <62601444+abdulrahman-haroon@users.noreply.github.com>
2020-08-28 18:05:15 +0500

    PROJECT COMPLETED

--- app/api/client.js
+++ app/api/client.js
@@ -4,7 +4,7 @@ import cache from "../utility/cache";
 import authStorage from "../auth/storage";
 
 const apiClient = create({
-  baseURL: "http://192.168.10.5:9000/api",
+  baseURL: "http://(your ipAddress):9000/api",
 });
 
 apiClient.addAsyncRequestTransform(async (request) => {
